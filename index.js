5481c8651caf39f55b2ff052af95e63c34ed3cf9
boudhibzied <zied.boudhib@esprit.tn>
2017-01-11 06:16:45 +0100

    redux dispatch() + seed.json

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import startServer from './src/server';
 export const store = makeStore();
 
 startServer(store);
+
+store.dispatch({
+  type: 'setEntries',
+  entries: require('./entries.json')
+});
+
+store.dispatch({type: 'NEXT'});
