606ed04cf606821511ad333fe7e6d1f7fd9f7ba4
boudhibzied <zied.boudhib@esprit.tn>
2017-01-16 02:33:24 +0100

    no message

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import Server from 'socket.io';
 
 const startServer = (store) => {
 
-	const io = new Server().attach(8080);
+	const io = new Server().attach(8090);
 
 	store.subscribe(
     	() => io.emit('state', store.getState().toJS())
